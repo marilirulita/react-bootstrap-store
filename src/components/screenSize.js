a568dc85d9884aabcfecdc8dfea184971555c74e
mar bautista <marilirulita@gmail.com>
2024-04-10 18:06:08 -0700

    create a function for knows the screen size

---
+++ src/components/screenSize.js
@@ -0,0 +1,21 @@
+'use client'
+import { useState, useEffect } from "react";
+
+export default function useScreenSize() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  return { height, width }
+}
