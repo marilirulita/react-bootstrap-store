c79f2ff0b3f6a65d25d92ecdb7223520e0c18cda
mar bautista <marilirulita@gmail.com>
2024-04-17 19:51:23 -0700

    add pagination stiling

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -152,14 +152,35 @@ export default function Page() {
               </div>
             ))}
           </div>
-          <div>
-            <form
-              class="input-page d-flex align-items-baseline justify-content-center"
-              action="index.html"
-              method="post"
-              id="page-count"
-            ></form>
-          </div>
+          
+          {/* pagination */}
+          <nav aria-label="...">
+            <ul className="pagination justify-content-center">
+              <li className="page-item disabled">
+                <a className="page-link">Previous</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li className="page-item active" aria-current="page">
+                <a className="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </main>
