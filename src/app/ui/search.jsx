ef9cc7d430ff49af9a77a2e69fa66ba76d850f20
mar bautista <marilirulita@gmail.com>
2024-04-22 21:28:22 -0700

    add replace path for update url path when search

--- src/app/ui/search.jsx
+++ src/app/ui/search.jsx
@@ -1,8 +1,20 @@
 'use client';
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
  
 export default function Search({ placeholder }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`)
   }
  
   return (
